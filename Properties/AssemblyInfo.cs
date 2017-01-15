4cf08f48aae3d09f1a91db2c8586e07eadbf313c
alishahb@users.noreply.github.com <34hh0sdifsd>
2017-01-15 18:47:11 +0300

    PICS

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
-[assembly: AssemblyVersion("1.1.7.1819")]
-[assembly: AssemblyInformationalVersion("Build date: 2017-01-15 18:19:17; Revision date: 2017-01-14 21:58:41; Revision(s) in working copy: 1:7; WARNING working copy had uncommitted modifications.")]
+[assembly: AssemblyVersion("1.1.9.1845")]
+[assembly: AssemblyInformationalVersion("Build date: 2017-01-15 18:45:00; Revision date: 2017-01-15 18:44:57; Revision(s) in working copy: 1:9.")]
