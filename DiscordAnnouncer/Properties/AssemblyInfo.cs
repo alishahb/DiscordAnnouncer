8784297e213f4863d786ed1bba4f80ec6767ed2c
alishahb@users.noreply.github.com <34hh0sdifsd>
2017-01-15 19:29:01 +0300

    appveyor

--- DiscordAnnouncer/Properties/AssemblyInfo.cs
+++ DiscordAnnouncer/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
-[assembly: AssemblyVersion("1.1.9.1910")]
-[assembly: AssemblyInformationalVersion("Build date: 2017-01-15 19:10:03; Revision date: 2017-01-15 18:44:57; Revision(s) in working copy: 1:9; WARNING working copy had uncommitted modifications.")]
+[assembly: AssemblyVersion("1.1.10.1927")]
+[assembly: AssemblyInformationalVersion("Build date: 2017-01-15 19:27:38; Revision date: 2017-01-15 19:25:10; Revision(s) in working copy: 1:10.")]
